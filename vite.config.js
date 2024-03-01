0378d224c30f5e3cc9b40aaf1752d155f072c3e6
ahmadilham_13 <ahmad13ilham13@gmail.com>
2024-03-01 20:52:02 +0700

    (+) integrate UI

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,14 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/sass/app.scss',
                 'resources/js/app.js',
+
+                'resources/views/themes/indotoko/assets/css/main.css',
+                'resources/views/themes/indotoko/assets/plugins/jqueryui/jquery-ui.css',
+
+                'resources/views/themes/indotoko/assets/js/main.js',
+                'resources/views/themes/indotoko/assets/plugins/jqueryui/jquery-ui.min.js',
             ],
             refresh: true,
         }),
